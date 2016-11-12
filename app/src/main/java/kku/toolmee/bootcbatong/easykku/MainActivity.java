34def0956582282d0bc7ad2e3e35ee043f5f67b6
bootcba <ibootcba@gmail.com>
2016-11-12 12:04:53 +0700

    ลิ้งค์ไปหน้า signin สำเร็จ

--- app/src/main/java/kku/toolmee/bootcbatong/easykku/MainActivity.java
+++ app/src/main/java/kku/toolmee/bootcbatong/easykku/MainActivity.java
@@ -1,16 +1,33 @@
 package kku.toolmee.bootcbatong.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+     //ประกาศตัวแปล
+    private Button signInButton, SignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bind Widger
+        signInButton = (Button) findViewById(R.id.button);
+        signInButton = (Button) findViewById(R.id.button2);
+         //Sign up Controler
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
 
 
 
-    }
-}
+    } //main method
+} //main class
